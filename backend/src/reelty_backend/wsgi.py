5d2d2a79deaf25bf785c97f5dbfbc91d2b55b6eb
Sai <156954638+Sai530-dot@users.noreply.github.com>
2025-12-01 06:16:06 -0600

    Modify WSGI for correct settings and path
    Updated the WSGI configuration to add the 'src' folder to the system path and set the correct Django settings module.

--- backend/src/reelty_backend/wsgi.py
+++ backend/src/reelty_backend/wsgi.py
@@ -1,7 +1,16 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-# CHANGE 'your_project_name' to the actual name of the folder containing settings.py
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+# 1. Add the 'src' folder to the system path so Python can find 'reelty_backend'
+# This gets the folder above the current one (which is 'src')
+current_path = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.dirname(current_path)
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+# 2. Set the correct settings module
+# CHANGE 'your_project_name' -> 'reelty_backend'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reelty_backend.settings')
 
 application = get_wsgi_application()
