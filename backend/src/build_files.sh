165aa8ada4d46bb8c5a254a5fdb299b3b3dd242e
Sai530-dot <156954638+Sai530-dot@users.noreply.github.com>
2025-12-06 02:14:47 -0600

    Configure Vercel backend build and routing

--- backend/src/build_files.sh
+++ backend/src/build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Collect static assets
-python3 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
